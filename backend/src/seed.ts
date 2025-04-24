6c3c1f8f7b86dfa6dc7514f2287080775db62286
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2025-04-24 16:24:53 +0200

    feat(seed): added interactive transaction
    Previously, if any model load failed, it would just output to stderr and the DB would come into an inconsistent state.

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -9,50 +9,44 @@ const AEON_FILE_NAME = 'model.aeon';
 
 const processedFiles = new Set();
 
-export const seed = async () => {
-    try {
+export const seed = () => {
+    return client.$transaction(async (tx) => {
         const subFolders = await fs.readdir(MODELS_DIR_PATH, { withFileTypes: true });
 
         for (const subFolder of subFolders) {
-            if (subFolder.isDirectory()) {
-                const metadataFilePath = path.join(MODELS_DIR_PATH, subFolder.name, METADATA_FILE_NAME);
-                const aeonFilePath = path.join(MODELS_DIR_PATH, subFolder.name, AEON_FILE_NAME);
-
-                if (!processedFiles.has(aeonFilePath)) {
-                    try {
-                        const metadataContents = await fs.readFile(metadataFilePath, 'utf-8');
-                        const metadata: ModelMetadata = ModelMetadataSchema.parse(JSON.parse(metadataContents));
-                        const aeonData = await fs.readFile(aeonFilePath);
-
-                        await client.model.create({
-                            data: {
-                                name: metadata.name,
-                                urlPublication: metadata['url-publication'],
-                                urlModel: Array.isArray(metadata['url-model'])
-                                    ? metadata['url-model'][0]
-                                    : metadata['url-model'],
-                                keywords: metadata.keywords,
-                                variables: metadata.variables,
-                                inputs: metadata.inputs,
-                                regulations: metadata.regulations,
-                                notes: metadata.notes,
-                                bib: metadata.bib,
-                                modelData: aeonData,
-                            },
-                        });
-
-                        processedFiles.add(aeonFilePath);
-                        console.log(`Imported data from ${subFolder.name}`);
-                    } catch (e) {
-                        console.error(`Error importing data from ${subFolder.name}`, e as Error);
-                    }
-                }
+            if (!subFolder.isDirectory()) {
+                continue;
             }
+
+            const metadataFilePath = path.join(MODELS_DIR_PATH, subFolder.name, METADATA_FILE_NAME);
+            const aeonFilePath = path.join(MODELS_DIR_PATH, subFolder.name, AEON_FILE_NAME);
+            if (processedFiles.has(aeonFilePath)) {
+                console.log(`Skipping ${metadataFilePath} since it's already been processed.`);
+                continue;
+            }
+
+            const metadataContents = await fs.readFile(metadataFilePath, 'utf-8');
+            const metadata: ModelMetadata = ModelMetadataSchema.parse(JSON.parse(metadataContents));
+            const aeonData = await fs.readFile(aeonFilePath);
+
+            await tx.model.create({
+                data: {
+                    name: metadata.name,
+                    urlPublication: metadata['url-publication'],
+                    urlModel: Array.isArray(metadata['url-model']) ? metadata['url-model'][0] : metadata['url-model'],
+                    keywords: metadata.keywords,
+                    variables: metadata.variables,
+                    inputs: metadata.inputs,
+                    regulations: metadata.regulations,
+                    notes: metadata.notes,
+                    bib: metadata.bib,
+                    modelData: aeonData,
+                },
+            });
+
+            processedFiles.add(aeonFilePath);
+            console.log(`Imported data from ${subFolder.name}`);
         }
         console.log('All the files were successfully imported!');
-    } catch (e) {
-        console.error('Error reading subFolders!', e as Error);
-    } finally {
-        await client.$disconnect();
-    }
+    });
 };
