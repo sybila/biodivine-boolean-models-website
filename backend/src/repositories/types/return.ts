5338df7fa401adb0c50aacd6c44797d07b4896af
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2025-04-25 10:22:31 +0200

    refactor!: removed all backend and DB dead code

--- backend/src/repositories/types/return.ts
+++ backend/src/repositories/types/return.ts
@@ -1,8 +1,5 @@
 import { Model } from '@prisma/client';
 import DbResult from './common';
 
-type DbModel = DbResult<Model>;
-
-export type ModelResult = DbModel;
 export type ModelReadSpecificResult = DbResult<Model>;
 export type ModelReadFilteredResult = DbResult<Model[]>;
--- backend/src/routes/modelRoutes.ts
+++ backend/src/routes/modelRoutes.ts
@@ -1,8 +1,5 @@
 import { Router } from 'express';
 import { readAllModelsController, readSpecificModelController } from '../controllers/model/read';
-// import {deleteModelController} from "../controllers/model/delete";
-// import {updateModelController} from "../controllers/model/update";
-// import {createModelController} from "../controllers/model/create";
 
 const modelRouter = Router();
 
@@ -11,10 +8,5 @@ const routerModelWithId = '/models/:id';
 
 modelRouter.get(routerModel, readAllModelsController);
 modelRouter.get(routerModelWithId, readSpecificModelController);
-
-// Route for creating new model - unused in this web app, possible to uncomment if needed later
-// modelRouter.post(routerModel, createModelController);
-// modelRouter.patch(routerModelWithId, updateModelController);
-// modelRouter.delete(routerModelWithId, deleteModelController);
 
 export default modelRouter;
